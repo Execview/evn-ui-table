7bb139544ed46ce2ff254dc8e36ae9b40f2b2c46
Namyts <35004248+Namyts@users.noreply.github.com>
2023-06-21 13:06:28 +0100

    altered number cell behaviour

--- src/cells/NumberCell/NumberCell.js
+++ src/cells/NumberCell/NumberCell.js
@@ -4,25 +4,31 @@ import classes from './NumberCell.module.css'
 
 const NumberCell =  props => {
 	const [errorText, setErrorText] = useState()
-	const { onValidateSave, data, prefix, placeholder, errorText: inputErrorText, onChange: inputOnChange,...otherProps} = props
-	const onSave = data => {
-		if(data){
-			onValidateSave && onValidateSave(parseFloat(data))
+	const { onValidateSave, data, prefix: inputPrefix, placeholder, errorText: inputErrorText, onChange: inputOnChange,...otherProps} = props
+	const onSave = numString => {
+		if(numString){
+			const num = parseFloat(numString)
+			if(data<=0 && data*-1===num){
+				setErrorText('This number has been set from -ve to +ve')
+			}
+			onValidateSave && onValidateSave(num)
 		} else {
 			setErrorText('Invalid number')
 		}
 	}
 	const onChange = e => {
+		console.log(e)
 		setErrorText(null)
 		inputOnChange && inputOnChange(e)
 	}
+	const prefix = `${data >= 0 ? '' : '- '}${inputPrefix}`
 	return (
 		<div className={classes['number-cell']}>
-			<div>{(data || data===0) && prefix}</div>
+			<div className={classes['prefix']}>{(data || data===0) && prefix}</div>
 			<TextCell {...{
 				...otherProps,
 				errorText: errorText || inputErrorText,
-				data,
+				data: Math.abs(data).toString(),
 				onChange,
 				placeholder: `Enter a number...`,
 				onValidateSave: onSave,
